2c2f2cb497dcb1979fdf16280887bdf164919cd3
saulat-ux <saulat.zubair@gmail.com>
2024-10-25 07:40:23 +0500

    completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-// script.js
 $(document).ready(function () {
-  // DOM manipulation example
-  $("#animateBtn").click(function () {
-    $("h1").animate({ fontSize: "3rem" }, 1000);
+  // Toggle the navbar when the burger icon is clicked
+  $(".burger-icon").click(function () {
+    $(".nav-links").toggleClass("active");
+    $(".btn-hide").toggleClass("active"); // Show/Hide the links
   });
+});
 
-  // Example of adding a class to an element for dynamic styling
-  $("nav").addClass("nav-styles");
+$(document).ready(function () {
+  $(".logo-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    speed: 1000,
+    arrows: false,
+    dots: true,
+  });
 });
